fda18cf9ffff6808784877c7e769409e1318bc88
tangjian <tangjian1891@163.com>
2022-04-30 12:32:47 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,13 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd .vitepress/dist
+# cd .vitepress/dist
+
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
-
-git init
+cp -r -f .vitepress/dist dist
+# git init
 git add -A
 git commit -m 'deploy'
 
@@ -21,5 +22,7 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+
+git push -f git@github.com:tangjian1891/docs.git master
 
 cd -
---
+++ dist/assets/手写实现_promise.md.b1ea3701.lean.js
@@ -0,0 +1,1 @@
+import{_ as n,c as s,o as a,b as p}from"./app.ef56ccaf.js";const d='{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"\u624B\u5199\u5B9E\u73B0/promise.md"}',t={},o=p("",3),c=[o];function e(u,l,k,i,r,m){return a(),s("div",null,c)}var f=n(t,[["render",e]]);export{d as __pageData,f as default};
